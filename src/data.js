e4d212edea02d60e92351c8e17fb2fd69e937f18
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2021-11-01 12:28:09 -0500

    Experimentando el fetch con el json

--- src/data.js
+++ src/data.js
@@ -1,9 +1,32 @@
 // estas funciones son de ejemplo
 
 export const example = () => {
-  return 'example';
+
+  let pokemon;
+  // return 'Este es el example';
+  fetch("./data/pokemon/pokemon.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data){
+
+      // console.log(data.pokemon[0].name);
+      pokemon = JSON.stringify(data.pokemon[0].name);
+      // console.log(data.pokemon[1].name);
+
+      // let item1 = document.getElementById("item1").innerHTML = data.pokemon[0].name;
+
+      
+    });
+    // return example;
+    // console.log(pokemon.stringify);
+    console.log(typeof pokemon);
+    // console.log("final del pokemon " + pokemon)
+    return pokemon;
 };
 
 export const anotherExample = () => {
   return 'OMG';
 };
+
+
--- src/main.js
+++ src/main.js
@@ -4,3 +4,9 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+// let pokemon = [{name:"lucero"},{name:"mirian"},{name:"diosito"}];
+// console.log(pokemon[0].name);
+
+// let prueba = {"pokemon" : [{"num": "001","name": "bulbasaur"},{"num": "002","name": "pikachu"}]};
+// console.log(prueba.pokemon[1].name);
