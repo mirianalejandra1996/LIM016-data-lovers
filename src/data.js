f2d28a1e3bb1772eb5b26d698e28a6e42b011726
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2021-11-05 08:58:38 -0500

    input funcional

--- src/data.js
+++ src/data.js
@@ -1,9 +1,62 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+let contenedorPokemon = document.getElementById('pokemones-filtrados');
+// let inputBuscar = document.getElementById('input-buscar');
+
+
+export const traerPokemones = (pokemon) => {
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.value}/`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data){
+      createPokemon(data);
+    })
+    // .catch((data){
+    //   notFound()
+    // });
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+
+function createPokemon (pokemon) {
+
+contenedorPokemon.innerHTML = `
+<div class="item">
+    <h2>N° ${pokemon.id}</h2>
+    <img src="${pokemon.sprites.front_default}" alt="" />
+    <h3>${formatName (pokemon.name)}</h3>
+</div>`
+}
+
+
+// function notFound (){
+//   contenedorPokemon.innerHTML = `Este pokemón no se encuentra`
+// }
+
+// function createPokemon (pokemon) {
+
+//   const img = document.createElement('img');
+//   img.src = pokemon.sprites.front_default;
+
+//   const tarjeta = document.createElement('div');
+//   tarjeta.classList.add('item');
+//   tarjeta.innerHTML = pokemon.name;
+//   tarjeta.innerHTML = pokemon.id;
+
+//   tarjeta.prepend(img);
+
+//   contenedorPokemon.prepend(tarjeta);
+
+//   // document.getElementById('pokemones-filtrados').prepend(tarjeta);
+  
+//   // document.getElementById('pokemones-filtrados').appendChild(tarjeta)
+// }
+
+function formatName (name) {
+  return name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+// function formatId (id){
+//   return id.toLocaleString()
+// }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import { traerPokemones } from './data.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+// let contenedorPokemon = document.getElementById(pokemones-filtrados);
+
+console.log(traerPokemones,data);
+
+let inputBuscar = document.getElementById('input-buscar');
+let btnBuscar = document.getElementById('btn-buscar');
+
+btnBuscar.addEventListener('click', ()=>{traerPokemones(inputBuscar)});
+
+// traerPokemones();
+
+
+
+// let pokemon = [{name:"lucero"},{name:"mirian"},{name:"diosito"}];
+// console.log(pokemon[0].name);
+
+// let prueba = {"pokemon" : [{"num": "001","name": "bulbasaur"},{"num": "002","name": "pikachu"}]};
+// console.log(prueba.pokemon[1].name);
